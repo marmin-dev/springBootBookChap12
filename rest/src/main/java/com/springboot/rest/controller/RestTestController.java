7bd9d34048bdf3ad9bc5c76234b7576c1463c684
Marmin <marmin.dev@gmail.com>
2022-12-24 01:16:25 +0900

    testRestTemplate

---
+++ rest/src/main/java/com/springboot/rest/controller/RestTestController.java
@@ -0,0 +1,41 @@
+package com.springboot.rest.controller;
+
+import com.springboot.rest.dto.MemberDto;
+import com.springboot.rest.service.RestTemplateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest-template")
+public class RestTestController {
+
+    private final RestTemplateService restTemplateService;
+
+    public RestTestController(RestTemplateService restTemplateService) {
+        this.restTemplateService = restTemplateService;
+    }
+
+    @GetMapping
+    public String getName(){
+        return restTemplateService.getName();
+    }
+    @GetMapping("/path-variable")
+    public String getNameWithPathVariable(){
+        return restTemplateService.getNameWithPathVariable();
+    }
+    @GetMapping("/parameter")
+    public String getNameWithParameters(){
+        return restTemplateService.getNameWithParameter();
+    }
+    @PostMapping
+    public ResponseEntity<MemberDto> postDto(){
+        return restTemplateService.postWithParamAndBody();
+    }
+    @PostMapping("/header")
+    public ResponseEntity<MemberDto> postWithHeader(){
+        return restTemplateService.postWithHeader();
+    }
+}
